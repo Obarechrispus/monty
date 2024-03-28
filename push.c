146b3da6be28a4540d31822d90e36c42950d5af1
Obarechrispus <obarechrispus@gmail.com>
2024-03-28 06:11:58 -0700

    task1

--- push.c
+++ push.c
@@ -2,7 +2,31 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+/*	char *arg = instruction_s->opcode;*/
+	int value = atoi(arg);
+	stack_t *new_node;
+
+
+	if (arg == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	if (*stack == NULL)
+	{
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = *stack;
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
 }
 
