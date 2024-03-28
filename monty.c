108c1e950eabf71afcca661ed4f34f2569a4abae
Obarechrispus <obarechrispus@gmail.com>
2024-03-28 12:51:35 -0700

    task1

--- monty.c
+++ monty.c
@@ -2,51 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	stack_t *stack = NULL;
-	char *line = NULL;
-	size_t len = 0;
-	unsigned int line_number = 0;
-	size_t read;
-	FILE *file;
+#define MAX_LINE_LENGTH 1024
 
-	(void)read;
-    if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    stack_t *stack = NULL;
+    char *line = NULL;
+   /* size_t len = MAX_LINE_LENGTH;*/
+    unsigned int line_number = 0;
+    FILE *file;
+
+    if (argc != 2) {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
 
-   
     file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
+    if (file == NULL) {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
-   /* while ((read = getline(&line, &len, file)) != -1)
-    {
+    line = malloc(MAX_LINE_LENGTH);
+    if (line == NULL) {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
         line_number++;
-      
-        push(&stack, line_number, line);
-        pall(&stack, line_number);       
-    }*/
-    while (fgets(line, len, file) != NULL) {
-        line_number++;
-        
-        if (line[0] != '\n') { 
+        if (line[0] != '\n') {
+           
+            size_t line_len = strlen(line);
+            if (line[line_len - 1] == '\n') {
+                line[line_len - 1] = '\0';
+            }
             push(&stack, line_number, line);
-            pall(&stack, line_number); 
+            pall(&stack, line_number);
         }
     }
 
-    
-    if (line)
-    {
-        free(line);
-    }
+    free(line);
     fclose(file);
 
     return EXIT_SUCCESS;
