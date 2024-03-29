c5cab4ffcbcd92b61f35f92fde9adf1a4a628d3c
ivyie1234 <osmerahyvonne@gmail.com>
2024-03-28 17:01:09 -0700

    k

--- monty.h
+++ monty.h
@@ -37,7 +37,7 @@ typedef struct instruction_s
 
 
 /* Function prototypes */
-void push(stack_t **stack, int value);
+void push(stack_t **stack, char *value, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 
--- push1.c
+++ push1.c
@@ -1,33 +1,46 @@
 #include "monty.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * push - Opcode function to push an element to the stack
+ * push - Pushes an element onto the stack
  * @stack: Double pointer to the head of the stack
- * @value: Value to be pushed to the stack
+ * @value: Value to push onto the stack
+ * @line_number: Line number in the Monty bytecode file
+ *
+ * Description: This function pushes an element onto the stack.
  */
-void push(stack_t **stack, int value)
+void push(stack_t **stack, char *value, unsigned int line_number)
 {
-    /* Create a new stack node */
+    int num;
     stack_t *new_node = malloc(sizeof(stack_t));
+
+    if (value == NULL)
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    num = atoi(value);
+    if (num == 0 && *value != '0')
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
     if (new_node == NULL)
     {
         fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
     }
 
-    new_node->n = value;
+    new_node->n = num;
     new_node->prev = NULL;
+    new_node->next = *stack;
 
-    if (*stack == NULL)
-    {
-        new_node->next = NULL;
-    }
-    else
-    {
-        new_node->next = *stack;
+    if (*stack != NULL)
         (*stack)->prev = new_node;
-    }
 
     *stack = new_node;
 }
@@ -41,6 +54,9 @@ void pall(stack_t **stack, unsigned int line_number)
 {
     stack_t *current = *stack;
     (void)line_number;
+
+    if (current == NULL)
+        return;
 
     while (current != NULL)
     {
